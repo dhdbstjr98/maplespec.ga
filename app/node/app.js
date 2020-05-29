b346b7768746202836652398c85c9a550015bd4c
오윤석 <dhdbstjr98@khu.ac.kr>
2020-05-29 23:44:19 +0900

    mariadb 삭제
    프로젝트 계획 변경으로 데이터베이스를 사용하지 않기로 함

--- app/node/app.js
+++ app/node/app.js
@@ -1,34 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-var db;
-
-fs.readFile('./config/db_password.secret', function(err, data) {
-	db = mysql.createConnection({
-	  host: "172.28.0.4",
-	  port: 3306,
-	  user: "root",
-	  password: data,
-	  database: "mysql"
-	});
-
-	db.connect();
-});
-
-app.get('/query', function (req, res) {
-    db.query("SHOW DATABASES",
-      function(err, results, fields) {
-        if (err) throw err;
-        var string = JSON.stringify(results);
-        res.send(string);
-      }
-    );
-});
 
 var server = app.listen(80);
