3b1ed2d92d66d9d76198633ecec2cc2f2368b839
오윤석 <dhdbstjr98@khu.ac.kr>
2020-06-04 21:50:35 +0900

    character page 추가

--- app/web/src/routes.js
+++ app/web/src/routes.js
@@ -1,7 +1,10 @@
 import Search from './routes/Search.svelte';
+import Character from './routes/Character.svelte';
 
 const routes = {
   '/': Search,
+  '/character': Character,
+  '/character/:character': Character,
 };
 
 export default routes;
---
+++ app/web/src/routes/Character.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let params = {};
+
+  import {push} from 'svelte-spa-router';
+
+  function init() {
+    if(!params.character) {
+      M.toast({html:"캐릭터명을 입력해주세요."});
+      push("/");
+    }
+  }
+
+  init();
+</script>
+
+<svelte:head>
+  
+</svelte:head>
