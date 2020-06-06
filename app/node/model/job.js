ea172c42522dd1d8de2b112467473d6429b75b9b
오윤석 <dhdbstjr98@khu.ac.kr>
2020-06-06 21:22:20 +0900

    직업, 스탯 model 추가

---
+++ app/node/model/job.js
@@ -0,0 +1,207 @@
+module.exports = {
+  '전사/히어로': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1.0746
+  },
+  '전사/팔라딘': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '전사/다크나이트': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '마법사/비숍': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1.2
+  },
+  '마법사/아크메이지(불,독)': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1.2
+  },
+  '마법사/아크메이지(썬,콜)': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1.2
+  },
+  '궁수/신궁': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '궁수/보우마스터': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '궁수/패스파인더': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '도적/섀도어': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '도적/나이트로드': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '도적/듀얼블레이더': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '해적/바이퍼': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '해적/캡틴': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '해적/캐논마스터': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '기사단/미하일': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '기사단/소울마스터': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '기사단/플레임위자드': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1.2
+  },
+  '기사단/윈드브레이커': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '기사단/나이트워커': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '기사단/스트라이커': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '아란/아란': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '에반/에반': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1
+  },
+  '루미너스/루미너스': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1
+  },
+  '메르세데스/메르세데스': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '팬텀/팬텀': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '은월/은월': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '레지스탕스/블래스터': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '레지스탕스/데몬슬레이어': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '레지스탕스/배틀메이지': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1
+  },
+  '레지스탕스/와일드헌터': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '레지스탕스/메카닉': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '카이저/카이저': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '카데나/카데나': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '엔젤릭버스터/엔젤릭버스터': {
+    'major': 'DEX',
+    'minor': 'STR',
+    'jobConst': 1
+  },
+  '아크/아크': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '일리움/일리움': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1
+  },
+  '아델/아델': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '제로/제로': {
+    'major': 'STR',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+  '키네시스/키네시스': {
+    'major': 'INT',
+    'minor': 'LUK',
+    'jobConst': 1
+  },
+  '호영/호영': {
+    'major': 'LUK',
+    'minor': 'DEX',
+    'jobConst': 1
+  },
+}
